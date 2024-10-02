d5b1ea37bace488edee3234eab3d25121c760868
Archana Roy <126496865+royarchana@users.noreply.github.com>
2024-10-02 10:05:52 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,31 @@
+let score = "33" // "hitesh" // null //undefined
+
+console.log(typeof (score))
+
+let value = Number(score)
+console.log(typeof value)
+console.log(value)
+/*
+"33" => 33
+"33abd" => NaN
+true => 1
+false => 0
+*/
+
+let isLogin ="Archana"
+
+let boolvalue = Boolean(isLogin)
+console.log(boolvalue)
+
+/*
+ 1 => true 
+ 0 => false
+ "" => false
+ "archana" => true
+ */
+
+ let num = 33
+
+ let str = String(num)
+ console.log(str)
+ console.log(typeof str)
