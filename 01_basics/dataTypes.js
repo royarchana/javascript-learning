b93067080ec952a58106f7936dc883b7cb74f339
Archana Roy <126496865+royarchana@users.noreply.github.com>
2024-10-02 12:15:35 +0000

    dataTypes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -27,7 +27,7 @@ console.log(typeof undefined) //undefined
 /*
 main reasons why data can be categories in two parts  => how data can be stored in memory and how can data should acssess
 
-1.Primitive => 7 Types: String , Number , Boolean ,null, undefined , symbol, BigInt
+1.Primitive => 7 Types: String , Number , Boolean ,null, undefined , symbol, BigInt*/
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
@@ -35,8 +35,8 @@ console.log(id === anotherId) // gives false
 
 const bigNumber = 123456789n
                         
-2.Non-Primitive or Refrence Type => Array ,Objects,Functions
-its datatypes is always object
+//2.Non-Primitive or Refrence Type => Array ,Objects,Functions
+ //its datatypes is always object
 
 const heros =["shaktiman","ironman"] // Array
 {
@@ -48,4 +48,26 @@ const myFunction = function(){
 console.log( "hello")
 }
 
-*/
+//***********************************************//
+// Stack(Primitive) - we get  copy of the declared variable
+ //Heap(Non-Primitive ) - we get the refrence value(original), changes may occur in original value.
+//example of stack
+ let myName ="archanaroy"
+ let anotherName = myName
+
+ anotherName ="avniroy"
+
+ console.log(myName) // value must be same
+ console.log(anotherName) // stores updated value
+
+ //example of heap
+ let userOne ={
+    email="user@gmai.com",
+    upi="user@ybl"
+ }
+
+ let userTwo = userOne
+ userTwo.email ="bail!gmai.com" // acssess by using '.'
+
+ console.log(userOne.email)
+ console.log(userTwo.email)
