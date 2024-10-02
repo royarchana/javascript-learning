1fe59f42f68cdd5685ea54fe62dba9d138126404
Archana Roy <126496865+royarchana@users.noreply.github.com>
2024-10-02 10:47:34 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,17 @@
+console.log("2" >1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+/*
+The reason  is that ans equality check == and comparisons > <>= <= work differently
+Comparisons convert null to a number,treating it as 0.
+That's why (3) null >= 0 is true and (1) null > 0 is false
+*/
+
+// === (without conversion check that equals or not)//
+
+console.log("2" == 2)
+console.log("2" === 2)
