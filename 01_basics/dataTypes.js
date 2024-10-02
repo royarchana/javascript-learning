876168d9575e29db4b9ed28f153d4219a321a3b7
Archana Roy <126496865+royarchana@users.noreply.github.com>
2024-10-02 07:31:26 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert(3+3) // this file not run as we are using nodejs,not browser
+
+console.log(3+3); console.log("hello") // this violates readability.
+
+let name ="archana" // string datatype
+let age = 18  // number datatype
+let isLogin = true// boolean datatype
+
+/* Primitive Datatypes
+number => 2 to power 53 
+bigint
+string => ""
+boolean  => true/false
+null -> standalone value
+undefined ->
+symbol -> unique
+
+object 
+
+*/
+// typeof -> tells the type of datatype 
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
