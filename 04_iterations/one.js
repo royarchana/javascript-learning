24a869dffcc11fdf6012264367482ef06cbbb174
Archana Roy <126496865+royarchana@users.noreply.github.com>
2024-10-13 10:06:54 +0000

    one

---
+++ 04_iterations/one.js
@@ -0,0 +1,21 @@
+// for loop
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    //console.log(element);   
+}
+// nested loop
+for (let i = 1; i < 10; i++) {
+    //console.log(`Outer loop value : ${i}`);
+    
+    for (let j = 1; j < 10; j++) {
+       // console.log(`Inner loop value : ${j} and inner loop ${i}`);
+        //console.log(i+ '*' +j+ '=' +i*j)
+    }   
+}
+let myArray =["flash","batman","superman"]
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    //console.log(element);     
+}
+
+// break and continue
