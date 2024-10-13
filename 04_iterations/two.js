521a75a7ee09535bd1dbdf69b84030c599c5ea33
Archana Roy <126496865+royarchana@users.noreply.github.com>
2024-10-13 10:06:39 +0000

    two

---
+++ 04_iterations/two.js
@@ -0,0 +1,22 @@
+// while (condition) {
+    
+// }
+let i =0;
+while (i<=10) {
+    console.log(`VAlue of index is ${i}`);
+    i+=2
+}
+
+let myArray =["flash","batman","superman"]
+
+let j=0;
+while(j<myArray.length){
+    console.log(`VAlue of index is ${myArray[j]}`);
+    j++
+}
+// do while loop -> executes at leat 1 times
+let score=11
+do {
+   console.log(`Score is ${score}`);
+   score++ 
+} while (score <=10);
